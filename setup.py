02ef1979f49185596ee76b5024004d18339cb4f5
armanhabib353 <armanhabib353@gmail.com>
2023-09-26 20:34:46 +0600

    detecting research

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '1.0.0',
+    author= 'S.M. Arman Habib',
+    author_email= 'armanhabibmain@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
