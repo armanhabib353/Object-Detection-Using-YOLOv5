ea835021e17700d05c0d0207f3f4652b64fccc7f
armanhabib353 <armanhabib353@gmail.com>
2023-10-01 13:47:10 +0600

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
+from typing import List
 from setuptools import find_packages, setup
 
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    # this function will return the list of requirements
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
 setup(
-    name = 'wasteDetection',
-    version= '1.0.0',
-    author= 'S.M. Arman Habib',
-    author_email= 'armanhabibmain@gmail.com',
-    packages= find_packages(),
-    install_requires = []
+    name="wasteDetection",
+    version="1.0.0",
+    author="Arman Habib",
+    author_email="armanhabib353@gmail.com",
+    packages=find_packages(),
+    install_require=get_requirements('requirements.txt')
 
 )
---
+++ wasteDetection/utils/main_utils.py
@@ -0,0 +1,37 @@
+import os.path
+import sys
+import yaml
+import base64
+
+from wasteDetection.exception import AppException
+from wasteDetection.logger import logging
+
+
+
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            logging.info("Read yaml file successfully")
+            return yaml.safe_load(yaml_file)
+
+    except Exception as e:
+        raise AppException(e, sys) from e
+    
+
+
+
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+            logging.info("Successfully write_yaml_file")
+
+    except Exception as e:
+        raise AppException(e, sys)
